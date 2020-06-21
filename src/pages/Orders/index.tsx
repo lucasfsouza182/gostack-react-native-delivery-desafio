fc75e8890ba6030272c8613f749d79118bcdbf4e
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-06-21 20:03:02 -0300

    Order page

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,16 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const { data } = await api.get<Food[]>('orders');
+
+      const formattedOrders = data.map(order => {
+        return {
+          ...order,
+          formattedValue: formatValue(order.price),
+        };
+      });
+
+      setOrders(formattedOrders);
     }
 
     loadOrders();
